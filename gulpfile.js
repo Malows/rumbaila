d1b11353f982a5ecf8413a6135a9811647e35800
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2016-06-21 16:13:25 -0300

    gulp builded

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+
+//var uncss = require('gulp-uncss');
+var sass = require('gulp-sass');
+var nano = require('gulp-cssnano');
+var cleanCss = require('gulp-clean-css');
+
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+
+var htmlmin = require('gulp-htmlmin');
+
+var rev = require('gulp-rev');
+var del = require('del');
+
+
+gulp.task('watch', function() {
+ gulp.watch('js/*.js', ['pack-js']);
+ gulp.watch('scss/*.scss', ['sass']);
+ gulp.watch('css/*.css', ['pack-css']);
+ gulp.watch('*.html', ['pack-html']);
+});
+
+//        //   /////
+//        //  //   //
+//        //   ///
+//        //      ///
+//  //   //   //   //
+//   ////      ////
+gulp.task('clean-js', function () {
+	return del([
+		'build/js/*.js'
+	]);
+});
+
+gulp.task('pack-js', ['clean-js'], function () {
+    return gulp.src([ 'lib/jquery/dist/jquery.js' ,'lib/bootstrap/dist/js/bootstrap.js', 'js/*.js' ])
+        .pipe(concat('bundle.js'))
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(uglify())
+        //.pipe(rev())
+        //.pipe(gulp.dest(''))
+        //.pipe(rev.manifest('build/rev-manifest.json', {
+        //    merge: true
+        //}))
+        .pipe(gulp.dest('build/js'));
+    });
+
+    //   ////      /////     ////
+    //  //  //    //   //  //   //
+    //  //         ///      ///
+    //  //           ///       ///
+    //  //  //    //   //   //   //
+    //   ////      ////       ////
+
+    gulp.task('clean-css', function () {
+    	return del([
+    		'build/css/*.css'
+    	]);
+    });
+
+    gulp.task('sass', function(){
+      return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+    });
+
+gulp.task('pack-css', ['clean-css'], function () {
+    return gulp.src(['lib/bootstrap/dist/css/bootstrap.css', 'lib/font-awesome/css/font-awesome.css' ,'css/grayscale.css'])
+        .pipe(concat('stylesheet.css'))
+        .pipe(nano())
+        .pipe(cleanCss())
+        //.pipe(rev())
+        .pipe(gulp.dest('build/css'))
+        /*.pipe(rev.manifest('build/rev-manifest.json', {
+            merge: true
+        }))
+        .pipe(gulp.dest(''));*/
+});
+
+
+// //    //  //////////  //\\  ////  //
+// //    //      //      // \\// //  //
+// ////////      //      //  \/  //  //
+// //    //      //      //      //  //
+// //    //      //      //      //  //////
+gulp.task('pack-html', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('pack-fonts', function() {
+  return gulp.src(['lib/bootstrap/fonts/*', 'lib/font-awesome/fonts/*'])
+  .pipe(gulp.dest('build/fonts'))
+})
+
+gulp.task('default', ['watch']);
--- js/grayscale.js
+++ js/grayscale.js
@@ -1,9 +1,3 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
@@ -15,6 +9,7 @@ function collapseNavbar() {
 
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
+
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -57,7 +52,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -169,7 +164,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
